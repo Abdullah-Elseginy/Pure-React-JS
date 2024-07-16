ee5608260630a451c3b38bd7bd32f10c8a8d49bd
Abdullah Elseginy <abdullah.ahmed.elseginy@gmail.com>
2024-07-16 21:50:05 +0300

    FetchDatauseEffect

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,18 +1,38 @@
 // import React from 'react';
-const Pet = (props) => {
+const Pet = ({ name, animal, bread }) => {
   // return React.createElement('div', {}, [
   //   React.createElement('h1', {id: props.id}, props.name),
   //   React.createElement('h2', {id: props.id}, props.type),
   //   React.createElement('h3', {id: props.id}, props.category),
   // ]);
+  // const { name, animal, bread } = props;
   return (
     <>
-      <div>
-        <h2> {props.name} </h2>
-        <h2> {props.type} </h2>
-        <h3> {props.category} </h3>
+      <div
+        style={{
+          display: 'flex',
+          backgroundColor: '#0cc',
+          borderRadius: 10,
+          justifyContent: 'space-around',
+          flexDirection: 'row',
+          margin: 10,
+        }}
+      >
+        <h2> {name} </h2>
+        <h2> {animal} </h2>
+        <h3> {bread} </h3>
       </div>
     </>
   );
 };
 export default Pet;
+
+// http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed},
+// pets api
+
+//         http://pets-v2.dev-apis.com/breeds?animal=${animal},
+
+// breeds api
+
+// default image
+// "http://pets-images.dev-apis.com/pets/none.jpg"
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,13 +1,33 @@
-// import React from 'react';
-
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Pet from './Pet';
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'rabitle'];
+const Breads = [];
 const Search = function () {
   const [location, Setlocation] = useState('Tanta');
   const [animal, setanimal] = useState('');
+  const [bread, setbread] = useState('');
+  const [pets, setpets] = useState([]);
+  useEffect(() => {
+    requestPets();
+  }, []);
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${bread}`,
+    );
+    const json = await res.json();
+    console.log(json.pets);
+    setpets(json.pets);
+  }
   return (
-    <div className="search-params">
-      <form>
+    <div className="search-params" style={{}}>
+      <form
+        style={{
+          backgroundColor: '#0cc',
+          // position: 'absolute',
+          // display: 'block',
+          alignSelf: 'center',
+        }}
+      >
         <label htmlFor="location">
           <input
             value={location}
@@ -37,8 +57,40 @@ const Search = function () {
             ))}
           </select>
         </label>
+
+        <label htmlFor="bread">
+          Animal
+          <select
+            id="bread"
+            value={bread}
+            onChange={(e) => {
+              setanimal(e.target.value);
+            }}
+          >
+            <option></option>;
+            {Breads.map((item, index) => (
+              <>
+                <option value={item} key={item}>
+                  {item}{' '}
+                </option>
+              </>
+            ))}
+          </select>
+        </label>
         <button>submmit</button>
       </form>
+      <div style={{}}>
+        {pets.map((item, index) => (
+          <>
+            <Pet
+              name={item.name}
+              animal={item.animal}
+              bread={item.breed}
+              key={item.id}
+            ></Pet>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
